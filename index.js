68980c7850501bc991ef6da714d1453473730ce4
jilvins <59861669+jilvins@users.noreply.github.com>
2020-12-03 22:02:04 +0200

    Game function basics
    Startet to update game functions in order to be able to change questions.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let currentPicture = document.querySelector('img')
 let answer = document.getElementById('option1')
+let nextButton = document.querySelector(".hidden2")
 let begginingScore = 0
 const score = document.getElementById('score')
 
@@ -23,44 +24,77 @@ answer.addEventListener('click', function () {
  document.getElementById('answer').innerText = 'You are right!!!'
  begginingScore++
  score.textContent = begginingScore
+ nextButton.classList.remove('hidden2')
 })
 
+document.querySelector('.nextButton').addEventListener ('click', function () {
+    currentPicture.src='/images/cat-2083492_1280.jpg'
+    
+    answer.innerHTML = cat.options[0];
+    document.getElementById('option2').innerHTML = cat.options[1];
+    document.getElementById('option3').innerHTML = cat.options[2];
+    document.getElementById('option4').innerHTML = cat.options[3];
+} )
 
 
 
 
 
-const cat = {
+const questions = [
+{
     english: 'cat',
     latvian: 'kaķis',
     image: '/images/cat-2083492_1280.jpg',
-    options: ['kaķis', 'kalvis', 'vāvere', 'vanags']
-}
+    options: [  {answer: 'kaķis', right: true}, 
+                {answer:'kalvis', right: false},    
+                {answer: 'vāvere', right: false}, 
+                {answer: 'vanags', right: false}
+            ],
 
-const dog = {
+        },
+{
     english: 'dog',
     latvian: 'suns',
     image: '/images/puppy-1903313_1280.jpg',
-    options: ['suns', 'sūna', 'purns', 'purvs']
-}
-
-const bread = {
+    options: [  {answer:'suns', right: true},
+                {answer:'sūna', right: false},
+                {answer:'purns', right: false},
+                {answer:'purvs', right: false}
+    ]
+},
+ {
     english: 'bread',
     latvian: 'maize',
     image: '/images/bread-1510155_1280.jpg',
-    options: ['maize', 'maiss', 'balva', 'baļķis']
-}
+    options: [  {answer:'maize', right: true},
+                {answer:'maiss', right: false},
+                {answer:'balva', right: false},
+                {answer:'baļķis', right: false}
+            ]
+},
 
-const water = {
+{
     english: 'water',
     latvian: 'ūdens',
     image: '/images/drip-921067_1280.jpg',
-    options: ['ūdens', 'ūdrs', 'ābols', 'āzis']
-}
+    options: [  {answer:'ūdens', right: true}, 
+                {answer:'ūdrs', right: false}, 
+                {answer:'ābols', right: false}, 
+                {answer:'āzis', right: false}
+            ]
+},
 
-const house = {
+ {
     english: 'house',
     latvian: 'māja',
     image: '/images/architecture-1836070_1280.jpg',
-    options: ['māja', 'mākonis', 'roka', 'ronis']
-}
+    options: [  {answer:'māja', right: true}, 
+                {answer:'mākonis', right: false},
+                {answer:'roka', right: false}, 
+                {answer:'ronis', right: false}
+            ]
+ }
+]
+
+
+
