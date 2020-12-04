f36176ebfd4830f285ab65e15a16b8ca72eb36c9
jilvins <59861669+jilvins@users.noreply.github.com>
2020-12-04 20:33:05 +0200

    Random word selector added
    Added functionality which allows to receive random word images and answers.
    Next I have to resolve the bug, where picture from previous word is still standing befero the new picture.

--- index.js
+++ index.js
@@ -1,100 +1,162 @@
-let currentPicture = document.querySelector('img')
-let answer = document.getElementById('option1')
-let nextButton = document.querySelector(".hidden2")
+let currentPicture = document.getElementById('wordPicture')
+let nextButton = document.querySelector(".nextButton")
 let begginingScore = 0
 const score = document.getElementById('score')
+const possibleWords = document.getElementById('option-buttons')
+let gameStarter = document.getElementById('gameStarter')
 
-let gameStarter = document.getElementById('gameStarter').addEventListener('click', function () {
+
+
+gameStarter.addEventListener('click', function () {
     let hiddenElements = document.querySelector(".hidden")
-    
+    gameStarter.classList.add('hidden')
     hiddenElements.classList.remove('hidden')
-    currentPicture.src='/images/architecture-1836070_1280.jpg'
-    
-    answer.innerHTML = house.options[0];
-    document.getElementById('option2').innerHTML = house.options[1];
-    document.getElementById('option3').innerHTML = house.options[2];
-    document.getElementById('option4').innerHTML = house.options[3];
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0
+    chooseNextWord()
 
     
     
 })
 
+nextButton.addEventListener('click', () => {
+    document.querySelector('img').classList.add('hidden')
+    currentQuestionIndex++
+    chooseNextWord ()
 
-answer.addEventListener('click', function () {
- document.getElementById('answer').innerText = 'You are right!!!'
+})
+
+let shuffledQuestions, currentQuestionIndex
+
+function chooseNextWord () {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])   
+}
+
+function showQuestion(picture) {
+    const imgElement = document.createElement('img')
+    imgElement.src = picture.url
+    currentPicture.append(imgElement)
+    picture.options.forEach(text => {
+        const button = document.createElement('button')
+        button.innerText = text.answer
+        button.classList.add('wordButton')
+        if (text.right) {
+            button.dataset.right = text.right
+        }
+        button.addEventListener('click', chooseAnswer)
+        possibleWords.appendChild(button)
+    })
+}
+
+
+function resetState () {
+clearStatusClass(document.body)
+ nextButton.classList.add('hidden')
+ while (possibleWords.firstChild) {
+     possibleWords.removeChild(possibleWords.firstChild)
+ }
+}
+
+function chooseAnswer (e) {
+    const selectedButton = e.target
+    const right = selectedButton.dataset.right
+    setStatusClass(document.body, right)
+    Array.from(possibleWords.children).forEach(button =>{
+        setStatusClass(button, button.dataset.right)   
+    })
+    if(shuffledQuestions.length > currentQuestionIndex + 1)
+    
+    begginingScore++
+    score.textContent = begginingScore
+    let messageCorrect = document.getElementById('correct')
+
+    let paragraphElement = document.createElement('p')
+    paragraphElement.innerHTML = 'You are right!!!'
+    messageCorrect.append(paragraphElement)                                 //Why this is not working?
+        nextButton.classList.remove('hidden')
+}
+function setStatusClass (element, right) {
+    clearStatusClass(element)
+    if (right) {
+        element.classList.add('right')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function  clearStatusClass(element) {
+    element.classList.remove('right')
+    element.classList.remove('wrong')
+}
+
+
+/*function nextLevel () {
+ document.getElementById('correct').innerText = 'You are right!!!'
  begginingScore++
  score.textContent = begginingScore
- nextButton.classList.remove('hidden2')
-})
-
-document.querySelector('.nextButton').addEventListener ('click', function () {
-    currentPicture.src='/images/cat-2083492_1280.jpg'
-    
-    answer.innerHTML = cat.options[0];
-    document.getElementById('option2').innerHTML = cat.options[1];
-    document.getElementById('option3').innerHTML = cat.options[2];
-    document.getElementById('option4').innerHTML = cat.options[3];
-} )
+ nextButton.classList.remove('hidden')
+}*/
 
 
 
 
 
 const questions = [
-{
-    english: 'cat',
-    latvian: 'kaķis',
-    image: '/images/cat-2083492_1280.jpg',
-    options: [  {answer: 'kaķis', right: true}, 
-                {answer:'kalvis', right: false},    
-                {answer: 'vāvere', right: false}, 
-                {answer: 'vanags', right: false}
-            ],
-
-        },
-{
-    english: 'dog',
-    latvian: 'suns',
-    image: '/images/puppy-1903313_1280.jpg',
-    options: [  {answer:'suns', right: true},
-                {answer:'sūna', right: false},
-                {answer:'purns', right: false},
-                {answer:'purvs', right: false}
+    {
+        english: 'cat',
+        latvian: 'kaķis',
+        url: '/images/cat-2083492_1280.jpg',
+        options: [  {answer: 'kaķis', right: true}, 
+                    {answer:'kalvis', right: false},    
+                    {answer: 'vāvere', right: false}, 
+                    {answer: 'vanags', right: false}
+                ],
+    
+            },
+    {
+        english: 'dog',
+        latvian: 'suns',
+        url: '/images/puppy-1903313_1280.jpg',
+        options: [  {answer:'suns', right: true},
+                    {answer:'sūna', right: false},
+                    {answer:'purns', right: false},
+                    {answer:'purvs', right: false}
+        ]
+    },
+     {
+        english: 'bread',
+        latvian: 'maize',
+        url: '/images/bread-1510155_1280.jpg',
+        options: [  {answer:'maize', right: true},
+                    {answer:'maiss', right: false},
+                    {answer:'balva', right: false},
+                    {answer:'baļķis', right: false}
+                ]
+    },
+    
+    {
+        english: 'water',
+        latvian: 'ūdens',
+        url: '/images/drip-921067_1280.jpg',
+        options: [  {answer:'ūdens', right: true}, 
+                    {answer:'ūdrs', right: false}, 
+                    {answer:'ābols', right: false}, 
+                    {answer:'āzis', right: false}
+                ]
+    },
+    
+     {
+        english: 'house',
+        latvian: 'māja',
+        url: '/images/architecture-1836070_1280.jpg',
+        options: [  {answer:'māja', right: true}, 
+                    {answer:'mākonis', right: false},
+                    {answer:'roka', right: false}, 
+                    {answer:'ronis', right: false}
+                ]
+     }
     ]
-},
- {
-    english: 'bread',
-    latvian: 'maize',
-    image: '/images/bread-1510155_1280.jpg',
-    options: [  {answer:'maize', right: true},
-                {answer:'maiss', right: false},
-                {answer:'balva', right: false},
-                {answer:'baļķis', right: false}
-            ]
-},
-
-{
-    english: 'water',
-    latvian: 'ūdens',
-    image: '/images/drip-921067_1280.jpg',
-    options: [  {answer:'ūdens', right: true}, 
-                {answer:'ūdrs', right: false}, 
-                {answer:'ābols', right: false}, 
-                {answer:'āzis', right: false}
-            ]
-},
-
- {
-    english: 'house',
-    latvian: 'māja',
-    image: '/images/architecture-1836070_1280.jpg',
-    options: [  {answer:'māja', right: true}, 
-                {answer:'mākonis', right: false},
-                {answer:'roka', right: false}, 
-                {answer:'ronis', right: false}
-            ]
- }
-]
 
 
 
--- words.js
+++ words.js
@@ -1,35 +1,56 @@
-const cat = {
-    english: 'cat',
-    latvian: 'kaķis',
-    image: '/images/cat-2083492_1280.jpg',
-    options: ['kaķis', 'kalvis', 'vāvere', 'vanags']
-}
-
-const dog = {
-    english: 'dog',
-    latvian: 'suns',
-    image: '/images/puppy-1903313_1280.jpg',
-    options: ['suns', 'sūna', 'purns', 'purvs']
-}
-
-const bread = {
-    english: 'bread',
-    latvian: 'maize',
-    image: '/images/bread-1510155_1280.jpg',
-    options: ['maize', 'maiss', 'balva', 'baļķis']
-}
-
-const water = {
-    english: 'water',
-    latvian: 'ūdens',
-    image: '/images/drip-921067_1280.jpg',
-    options: ['ūdens', 'ūdrs', 'ābols', 'āzis']
-}
-
-const house = {
-    english: 'house',
-    latvian: 'māja',
-    image: '/images/architecture-1836070_1280.jpg',
-    options: ['māja', 'mākonis', 'roka', 'ronis']
-}
-
+//export const questions = (props) => {[
+    [
+        {
+        english: 'cat',
+        latvian: 'kaķis',
+        image: '/images/cat-2083492_1280.jpg',
+        options: [  {answer: 'kaķis', right: true}, 
+                    {answer:'kalvis', right: false},    
+                    {answer: 'vāvere', right: false}, 
+                    {answer: 'vanags', right: false}
+                ],
+    
+            },
+    {
+        english: 'dog',
+        latvian: 'suns',
+        image: '/images/puppy-1903313_1280.jpg',
+        options: [  {answer:'suns', right: true},
+                    {answer:'sūna', right: false},
+                    {answer:'purns', right: false},
+                    {answer:'purvs', right: false}
+        ]
+    },
+     {
+        english: 'bread',
+        latvian: 'maize',
+        image: '/images/bread-1510155_1280.jpg',
+        options: [  {answer:'maize', right: true},
+                    {answer:'maiss', right: false},
+                    {answer:'balva', right: false},
+                    {answer:'baļķis', right: false}
+                ]
+    },
+    
+    {
+        english: 'water',
+        latvian: 'ūdens',
+        image: '/images/drip-921067_1280.jpg',
+        options: [  {answer:'ūdens', right: true}, 
+                    {answer:'ūdrs', right: false}, 
+                    {answer:'ābols', right: false}, 
+                    {answer:'āzis', right: false}
+                ]
+    },
+    
+     {
+        english: 'house',
+        latvian: 'māja',
+        image: '/images/architecture-1836070_1280.jpg',
+        options: [  {answer:'māja', right: true}, 
+                    {answer:'mākonis', right: false},
+                    {answer:'roka', right: false}, 
+                    {answer:'ronis', right: false}
+                ]
+     }
+    ]
